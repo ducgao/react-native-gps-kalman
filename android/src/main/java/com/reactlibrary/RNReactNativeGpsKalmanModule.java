1d237f37b1382ad39e673efc85d885e8f694da3f
Duc Nguyen <ducgaogao4213@gmail.com>
2021-06-27 06:21:48 +0700

    for android. haha

--- android/src/main/java/com/reactlibrary/RNReactNativeGpsKalmanModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeGpsKalmanModule.java
@@ -1,22 +1,68 @@
 
 package com.reactlibrary;
 
+import android.location.Location;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.List;
+
+import mad.location.manager.lib.Commons.Utils;
+import mad.location.manager.lib.Loggers.GeohashRTFilter;
 
 public class RNReactNativeGpsKalmanModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNReactNativeGpsKalmanModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    private GeohashRTFilter geoHashRTFilter;
 
-  @Override
-  public String getName() {
-    return "RNReactNativeGpsKalman";
-  }
+    public RNReactNativeGpsKalmanModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "GpsKalman";
+    }
+
+    @ReactMethod
+    public void startSession() {
+        if (geoHashRTFilter == null) {
+            geoHashRTFilter = new GeohashRTFilter(Utils.GEOHASH_DEFAULT_PREC, Utils.GEOHASH_DEFAULT_MIN_POINT_COUNT);
+        }
+
+        geoHashRTFilter.reset(null);
+    }
+
+    @ReactMethod
+    public void process(Double latitude, Double longitude, Double altitude, Double timeStamp, Promise promise) {
+        Location loc = new Location("GpsKalman");
+        loc.setLatitude(latitude);
+        loc.setLongitude(longitude);
+        loc.setAltitude(altitude);
+        loc.setTime(timeStamp.longValue());
+
+        geoHashRTFilter.filter(loc);
+        List<Location> latestData = geoHashRTFilter.getGeoFilteredTrack();
+
+        if (latestData.isEmpty()) {
+            promise.reject("ERROR", "Can not correct the point");
+        } else {
+            Location lastLoc = latestData.get(latestData.size() - 1);
+            WritableMap objectResolve = Arguments.createMap();
+
+            objectResolve.putDouble("latitude", lastLoc.getLatitude());
+            objectResolve.putDouble("longitude", lastLoc.getLongitude());
+            objectResolve.putDouble("altitude", lastLoc.getAltitude());
+            objectResolve.putDouble("time", timeStamp);
+            
+            promise.resolve(objectResolve);
+        }
+    }
 }
